0225a552517bcc323bb271862c026fc4a1f0ce78
Agricog <mick@mickstevenson.com>
2025-12-23 18:09:42 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/api\.snaglog\.co\.uk\/.*/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'api-cache',
+              expiration: { maxEntries: 50, maxAgeSeconds: 86400 }
+            }
+          }
+        ]
+      },
+      manifest: {
+        name: 'SnagLog',
+        short_name: 'SnagLog',
+        theme_color: '#f97316',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
+  ]
 })
